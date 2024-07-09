f84a8de9f045357645f83268810cdaef20cfbcf3
Maria-E-Reis <162997717+Maria-E-Reis@users.noreply.github.com>
2024-07-09 11:39:19 -0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+   <h1>bem vindo</h1> 
+   
+   ola professor :D
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  color:rgb(81, 37, 151)
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
